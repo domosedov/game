e328f5fbc473563cd86e6d0c346aa7321b5f60ad
Aleksandr Grigorii <domosedov.dev@gmail.com>
2022-10-19 18:39:30 +0300

    add block

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as PIXI from "pixi.js";
 import "./style.css";
 
-// const aspectRatio = 9 / 16;
+const DEFAULT_SCALE = 0.4;
+// const ASPECT_RATIO = 9 / 16;
 
 const app = new PIXI.Application({
   width: 432,
@@ -13,62 +14,134 @@ document.body.appendChild(app.view);
 
 const loader = PIXI.Loader.shared;
 
+loader.baseUrl = "assets";
+
 loader
-  .add("road", "assets/road.png")
-  .add("car", "assets/car.png")
-  .add("panel", "assets/panel.png")
-  .add("wheel", "assets/wheel.png");
+  .add("road", "road.png")
+  .add("car", "car.png")
+  .add("panel", "panel.png")
+  .add("wheel", "wheel.png")
+  .add("block", "block.png");
 
 loader.load(setup);
 
-function setup(...args: unknown[]) {
-  console.log(args);
+function setup(
+  _loader: PIXI.Loader,
+  resources: PIXI.utils.Dict<PIXI.LoaderResource>
+) {
+  const roadTexture = resources["road"].texture;
+  const carTexture = resources["car"].texture;
+  const panelTexture = resources["panel"].texture;
+  const wheelTexture = resources["wheel"].texture;
+  const blockTexture = resources["block"].texture;
 
-  let carSpeed = 10;
+  let carSpeed = 6;
+  const speed = 8;
+
+  // Block
+  function createBlock() {
+    const sprite = new PIXI.Sprite(blockTexture);
+    sprite.scale.set(DEFAULT_SCALE);
+    return sprite;
+  }
 
   // Road
-  const roadTexture = PIXI.Texture.from("assets/road.png");
-  const road = new PIXI.TilingSprite(
-    roadTexture,
-    app.view.width,
-    app.view.height
-  );
-  app.stage.addChild(road);
+  function createRoad() {
+    const sprite = new PIXI.TilingSprite(
+      roadTexture!,
+      app.view.width,
+      app.view.height
+    );
+    sprite.tileScale.set(DEFAULT_SCALE);
+    return sprite;
+  }
 
-  road.tileScale.set(1 / 2.5, 1 / 2.5);
+  // Car
+  function createCar() {
+    const sprite = new PIXI.Sprite(carTexture);
+    sprite.scale.set(DEFAULT_SCALE);
+    return sprite;
+  }
+
+  // Panel
+  function createPanel() {
+    const sprite = new PIXI.Sprite(panelTexture);
+    sprite.scale.set(DEFAULT_SCALE);
+    sprite.anchor.set(1, 1);
+    sprite.position.set(app.view.width, app.view.height);
+    return sprite;
+  }
+
+  // Wheel
+  function createWheel() {
+    const sprite = new PIXI.Sprite(wheelTexture);
+    sprite.scale.set(DEFAULT_SCALE);
+    sprite.anchor.set(0.5, 0.5);
+    return sprite;
+  }
+
+  // Utils
+  function rectsIntersect(a: PIXI.Container, b: PIXI.Container) {
+    const aBox = a.getBounds();
+    const bBox = b.getBounds();
+    return (
+      aBox.x + aBox.width > bBox.x &&
+      aBox.x < bBox.x + bBox.width &&
+      aBox.y + aBox.height > bBox.y &&
+      aBox.y < bBox.y + bBox.height
+    );
+  }
+
+  // Init
+  const road = createRoad();
+  const car = createCar();
+  const panel = createPanel();
+  const wheel = createWheel();
+  const block = createBlock();
 
   function moveRoad() {
     road.tilePosition.y += carSpeed;
   }
 
-  // Panel
-  const panelSprite = PIXI.Sprite.from("assets/panel.png");
-  app.stage.addChild(panelSprite);
-  panelSprite.scale.set(0.4, 0.4);
-  panelSprite.position.set(0, 768 - panelSprite._texture.orig.height / 2.5);
+  function stopGame() {
+    const isIntersect = rectsIntersect(car, block);
+    console.log({ isIntersect });
+    if (isIntersect) {
+      carSpeed = 0;
+    }
+  }
 
-  // Car
-  const carSprite = PIXI.Sprite.from("assets/car.png");
-  carSprite.x = 90;
-  carSprite.y = 380;
-  carSprite.scale.set(1 / 2.5, 1 / 2.5);
-  app.stage.addChild(carSprite);
+  block.position.set(90, 0);
+  function moveBlock() {
+    const blockBottomBoard = block.position.y + block.height;
+    console.log(blockBottomBoard);
+    if (block.y > app.view.height) {
+      block.position.set(90, 0);
+    }
+    block.y += carSpeed;
+  }
 
-  // Wheel
-  const wheel = PIXI.Sprite.from("assets/wheel.png");
-  app.stage.addChild(wheel);
-  wheel.scale.set(0.4, 0.4);
-  wheel.anchor.set(0.5, 0.5);
+  car.x = 90;
+  car.y = 380;
+
   wheel.position.set(216, 650);
   wheel.interactive = true;
   wheel.buttonMode = true;
   wheel.on("pointerdown", () => {
-    if (carSprite.position.x === 90) {
-      carSprite.position.x = 240;
+    console.log(car.getBounds());
+    if (car.position.x === 90) {
+      car.position.x = 240;
     } else {
-      carSprite.position.x = 90;
+      car.position.x = 90;
     }
   });
+
+  // Add to game
+  app.stage.addChild(road);
+  app.stage.addChild(car);
+  app.stage.addChild(block);
+  app.stage.addChild(panel);
+  app.stage.addChild(wheel);
 
   const arrowKeys = {
     ArrowUp: "ArrowUp",
@@ -85,8 +158,6 @@ function setup(...args: unknown[]) {
     ArrowRight: false,
     ArrowUp: false,
   };
-
-  const speed = 5;
 
   document.addEventListener("keydown", (event) => {
     console.log(event);
@@ -106,18 +177,20 @@ function setup(...args: unknown[]) {
 
   function gameLoop() {
     if (pressedArrowKeys.ArrowUp) {
-      carSprite.y -= speed;
+      car.y -= speed;
     }
     if (pressedArrowKeys.ArrowDown) {
-      carSprite.y += speed;
+      car.y += speed;
       carSpeed = 0;
     }
     if (pressedArrowKeys.ArrowLeft) {
-      carSprite.x -= speed;
+      car.x -= speed;
     }
     if (pressedArrowKeys.ArrowRight) {
-      carSprite.x += speed;
+      car.x += speed;
     }
     moveRoad();
+    moveBlock();
+    stopGame();
   }
 }
